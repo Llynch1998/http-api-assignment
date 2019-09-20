8ea58b1511b803ea7fadd85304a01b76ba902989
Wyatt Lynch (RIT Student) <wal8273@ad.rit.edu>
2019-09-20 13:40:22 -0400

    les go

--- src/server.js
+++ src/server.js
@@ -11,8 +11,6 @@ const urlStruct = {
   GET: {
     '/': htmlHandler.getIndex,
     '/style.css': htmlHandler.getCSS,
-    '/getUsers': jsonHandler.getUsers,
-    '/updateUser': jsonHandler.updateUser,
     notFound: jsonHandler.notFound,
   },
   HEAD: {
@@ -23,13 +21,7 @@ const urlStruct = {
 
 
 const onRequest = (request, response) => {
-  const parsedUrl = url.parse(request.url);
-
-  if (urlStruct[request.method][parsedUrl.pathname]) {
-    urlStruct[request.method][parsedUrl.pathname](request, response);
-  } else {
-    urlStruct[request.method].notFound(request, response);
-  }
+  
 };
 
 http.createServer(onRequest).listen(port);
